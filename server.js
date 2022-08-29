e6b5523a420fcd7a0ab18df99624be046fc52205
adicerbo <dicerboantonio32@gmail.com>
2022-08-29 16:20:58 -0400

    greetings

---
+++ server.js
@@ -0,0 +1,14 @@
+// require packages/dependencies
+const express = require('express');
+const res = require('express/lib/response');
+const port = 3000
+const app = express();
+
+app.get("/greeting/:name", (req, res) => {
+    res.send("Hello " + req.params.name)
+})
+
+
+app.listen(port, function() {
+    // console.log("hello")
+})
