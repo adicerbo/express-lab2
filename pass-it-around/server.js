e8eeeb54a382f76386915561b35119015478f939
adicerbo <dicerboantonio32@gmail.com>
2022-08-29 21:21:17 -0400

    take one down

--- pass-it-around/server.js
+++ pass-it-around/server.js
@@ -3,11 +3,21 @@ const express = require('express');
 const res = require('express/lib/response');
 const port = 3001;
 const app = express();
-// let num = 99
+
 app.listen(3001);
 
-app.get("/", (req, res) => {
-    res.send("99 bottles of beer on the wall " + '<br> </br>' + 
-    "<a href='/pass'>Take one down, pass it around</a>")
-    
+app.get('/', (req, res) => {
+    res.send(`<h1>99 bottles of beer on the wall</h1>
+    <a href="/98">Again!<a/>`)
 });
+
+app.get("/:number_of_bottles", (req, res) => {
+    let number_of_bottles = req.params.number_of_bottles;
+    if (number_of_bottles > 0 ) {
+        res.send(`<h1>${number_of_bottles} bottles of beer on the wall</h1>
+        <a href="/${number_of_bottles - 1}">Again!</a>`)
+    } else {
+        res.send(`<h1>No more beers!</h1><a href="/">Restart!</a>`)
+    }
+});
+
